ab27244bf82a68132c863cd8fe66c1a5daa25fb4
Даниил Дарьин ИУ10-35 <tutuit07@mail.ru>
2021-11-10 13:19:31 +0300

    RK-2

---
+++ src/useinputState.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+    const [value, setValue] = useState('');
+
+    return {
+        value,
+        onChange: event => {
+            setValue(event.target.value);
+        },
+        reset: () => setValue('')
+    };
+};
